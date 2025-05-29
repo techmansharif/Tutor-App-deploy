58247b035dee80f666ba66aec1366fc4154a8952
dibbo dey <dibbodey6000@gmail.com>
2025-05-29 10:37:02 +0600

    this branch localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,4 @@
 
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from app.router import quizzes
-# from pydantic import BaseModel
-# import uvicorn
-# import os 
-
-
-# app = FastAPI()
-
-# # Allow frontend (React) running on localhost:3000 to talk to backend
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000",
-#                    "https://test-deployment-e19fb.web.app",
-#                    "https://test-deployment-e19fb.firebaseapp.com"],  
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-
-# app.include_router(quizzes.router)
-
-
-# if __name__ == "__main__":
-#     # Use the PORT environment variable provided by Cloud Run, default to 8000
-#     port = int(os.getenv("PORT", 8000))
-#     uvicorn.run(app, host="0.0.0.0", port=port)
-
 
 
 #####################################
@@ -184,7 +154,7 @@ app.add_middleware(
     secret_key=SECRET_KEY,
     max_age=60*60,
     same_site="none",
-    https_only=True
+    https_only=False
 )
 
 
@@ -282,7 +252,7 @@ async def auth_callback(request: Request):
         
         request.session.pop("oauth_state", None)
         
-        return RedirectResponse(url="https://brimai-test-v1.web.app")
+        return RedirectResponse(url="http://localhost:3000")
     finally:
         db.close()
 
